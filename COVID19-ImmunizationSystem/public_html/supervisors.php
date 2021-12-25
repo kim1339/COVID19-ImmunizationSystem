ff7ee572744a36eebd1a840c04d4980f8ee6a0ab
Kimia Rostin <40971424+kim1339@users.noreply.github.com>
2021-12-25 15:55:29 -0800

    refactoring base functions

--- COVID19-ImmunizationSystem/public_html/supervisors.php
+++ COVID19-ImmunizationSystem/public_html/supervisors.php
@@ -88,72 +88,7 @@
     </div><br>
     
     <?php
-        $success = False;
-        $db_conn = NULL;
-
-        function executePlainSQL($cmdstr) { 
-            global $db_conn, $success;
-            $statement = OCIParse($db_conn, $cmdstr); 
-            if (!$statement) {
-                echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-                $e = OCI_Error($db_conn);
-                echo htmlentities($e['message']);
-                $success = False;
-            }
-            $r = OCIExecute($statement, OCI_DEFAULT);
-            if (!$r) {
-                echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-                $e = oci_error($statement);
-                echo htmlentities($e['message']);
-                $success = False;
-            }
-			return $statement;
-		}
-
-        function executeBoundSQL($cmdstr, $list) {
-			global $db_conn, $success;
-			$statement = OCIParse($db_conn, $cmdstr);
-
-            if (!$statement) {
-                echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-                $e = OCI_Error($db_conn);
-                echo htmlentities($e['message']);
-                $success = False;
-            }
-
-            foreach ($list as $tuple) {
-                foreach ($tuple as $bind => $val) {
-                    OCIBindByName($statement, $bind, $val);
-                    unset ($val); 
-				}
-                $r = OCIExecute($statement, OCI_DEFAULT);
-                if (!$r) {
-                    echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-                    $e = OCI_Error($statement);
-                    echo htmlentities($e['message']);
-                    echo "<br>";
-                    $success = False;
-                }
-            }
-        }
-
-        function connectToDB() {
-            global $db_conn;
-            $db_conn = OCILogon("", "", ""); // NEEDS TO BE FILLED W/ ORACLE ACCOUNT INFO
-
-            if ($db_conn) {
-                return true;
-            } else {
-                $e = OCI_Error();
-                echo htmlentities($e['message']);
-                return false;
-            }
-        }
-
-        function disconnectFromDB() {
-            global $db_conn;
-            OCILogoff($db_conn);
-        }
+        require __DIR__ . '/functions.php';
 
         function handleRemoveRequest() {
             global $db_conn;
